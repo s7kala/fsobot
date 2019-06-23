6286bbce76af5d1ab86fb4502a5aba94d26437d0
Siddharth Kala <51789867+s7kala@users.noreply.github.com>
2019-06-24 04:28:13 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -31,20 +31,9 @@ module.exports = (client, message) => {
 	if(message.content === '~leave') {
 		return leave(message);
 	}
-	const serverQueue = queue.get(message.guild.id);
-	// Play
-	const play = require('../commands/play');
-	if(message.content.startsWith('~play')) {
-		return play(message, serverQueue);
-	}
-	// Skip
-	const skip = require('../commands/skip');
-	if(message.content === '~skip') {
-		return skip(message, serverQueue);
-	}
-	// Stop
-	const stop = require('../commands/stop');
-	if(message.content === '~stop') {
-		return stop(message, serverQueue);
+	// Music
+	const music = require('../features/music');
+	if(message.content.startsWith('~play') || message.content.startsWith('~stop') || message.content.startsWith('~skip')) {
+		return music(message);
 	}
 }
