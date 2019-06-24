24ff52c03efdecd505d077184285d168a347a993
Siddharth Kala <51789867+s7kala@users.noreply.github.com>
2019-06-24 16:37:10 +0530

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, member) => {
-	 // Find channel to greet in
-  const channel = member.guild.channels.find(ch => ch.name === 'announcements');
-  if(!channel) return; // If channel doesn't exist, do nothing
-  channel.send(`Welcome to the server, ${member.user.tag}`);
-}
